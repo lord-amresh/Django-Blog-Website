1516632d4659021d955b920a06a7fabcd36310b4
lord-amresh <amresh2006@gmail.com>
2025-05-21 17:08:41 +0545

    Removed unnecesary apps

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
-    'blog',
-    'checkout'
+    'blog'
 ]
 
 MIDDLEWARE = [
