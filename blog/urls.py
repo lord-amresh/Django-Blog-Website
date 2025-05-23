95414001c900e085242ce475e523258f428efef1
lord-amresh <amresh2006@gmail.com>
2025-05-23 08:34:04 +0545

    routes first level complete

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import renderRegisterForm,renderLoginForm
+
+
+urlpatterns = [
+    path('register', renderRegisterForm),
+    path('login', renderLoginForm)
+]
