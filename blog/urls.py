6535dcf6adb07cbdcdf365b28fa12fec7115bdae
lord-amresh <amresh2006@gmail.com>
2025-05-25 08:32:17 +0545

    making UI elements

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import renderRegisterForm,renderLoginForm
+from .views import renderRegisterForm,renderLoginForm,renderHomePage
 
 
 urlpatterns = [
+    path('',renderHomePage),
     path('register', renderRegisterForm),
     path('login', renderLoginForm)
 ]
